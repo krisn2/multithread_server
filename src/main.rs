74d9d87aed6e2b9c7bed7a1f15c0b1e97715afa3
krisn2 <krishnasarone08@gmail.com>
2025-03-03 14:28:33 +0530

    SSR in rust tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream}, 
 };
@@ -14,12 +15,35 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let http_req:Vec<_> = buf_reader
-        .lines()
-        .map(|result|result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-    let res = "HTTP/1.1 200 OK\r\n\r\n";
-   stream.write_all(res.as_bytes()).unwrap();
-   println!("{:#?}", http_req);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    // let http_req:Vec<_> = buf_reader
+    //     .lines()
+    //     .map(|result|result.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
+
+    if request_line == "GET / HTTP/1.1" {
+    let status = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("src/hello.html").unwrap();
+    let length = contents.len();
+
+    let response = 
+    format!("{status}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+    }
+    else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("src/404.html").unwrap();
+        let length = contents.len();
+
+        let response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+        );
+
+        stream.write_all(response.as_bytes()).unwrap();
+    }
+
+//     let res = "HTTP/1.1 200 OK\r\n\r\n";
+//    println!("{:#?}", http_req);
 }
