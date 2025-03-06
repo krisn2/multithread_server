2e98eb2eb80f89cc962f2264f03b7d50c50eba17
krisn2 <krishnasarone08@gmail.com>
2025-03-06 11:11:29 +0000

    again

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,22 @@
-use std::thread;
+use std::{sync::mpsc, thread};
+
 pub struct ThreadPool {
    workers: Vec<Worker>,
+   sender: mpsc::Sender<Job>
 }
+
+struct Job ;
 
 impl ThreadPool {
     pub fn new (size:usize)-> ThreadPool {
         assert!(size > 0);
+        let (sender, receiver) = mpsc::channel();
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
             workers.push(Worker::new(id));
         }
-        ThreadPool {workers}
+        ThreadPool {workers,sender}
     }
     pub fn execute<F>(&self, f:F)
     where 
