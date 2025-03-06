da8b6e3ccfb40d40582d9255ef1fb2f5fa74a6ce
krisn2 <krishnasarone08@gmail.com>
2025-03-06 11:42:17 +0000

    impl exxcute fn and change job struct to type

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,33 @@
-use std::{sync::mpsc, thread};
+use std::{
+    sync::{mpsc,Arc,Mutex },
+    thread};
 
 pub struct ThreadPool {
    workers: Vec<Worker>,
    sender: mpsc::Sender<Job>
 }
 
-struct Job ;
+type Job = Box<dyn FnOnce() + Send + 'static >;
 
 impl ThreadPool {
     pub fn new (size:usize)-> ThreadPool {
         assert!(size > 0);
         let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id,Arc::clone(&receiver)));
         }
         ThreadPool {workers,sender}
     }
     pub fn execute<F>(&self, f:F)
     where 
         F:FnOnce() +Send + 'static,
-        {
-
-        }
+    {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
 }
 
 pub struct Worker {
@@ -32,8 +36,10 @@ pub struct Worker {
 }
 
 impl Worker {
-    pub fn new (id:usize)-> Worker {
-        let thread = thread::spawn(|| {});
+    pub fn new (id:usize, receiver:Arc<Mutex<mpsc::Receiver<Job>>>)-> Worker {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
 
         Worker{id,thread}
     }
